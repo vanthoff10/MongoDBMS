32349f4a18cea93aac18c9b71a650fad77aca0bf
Ashwin Panwar <ashwinpanwar1019999@gmail.com>
2020-09-21 23:58:05 +0530

    Advanced Tokenization through jwt

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -31,20 +32,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({})
-        res.status(201).send(users)
-    } catch(e) {
-        res.status(400).send(e)
-    }
-
-    // User.find({ }).then((users) => {
-    //     res.send(users)
-    // }).catch(() => {
-    //     res.status(500).send()
-    // })
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
